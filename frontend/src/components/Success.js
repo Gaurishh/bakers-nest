65a463672d7a74ff6d9f93cf43dcd972541d9f00
Gaurish Sood <9gaurish@gmail.com>
2026-01-04 17:54:49 +0530

    Phone no

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -7,7 +7,7 @@ const Success = ({ success }) => {
         <p>{success}</p>
         <p>The pick-up address is: 2103(3rd Floor), Sector 57, Gurgaon </p>
         <p>You will be notified on your email once your order has been prepared, which usually takes 1 business day. </p>
-        <p>For any queries or concerns, call/drop a message to 8595714343 </p>
+        <p>For any queries or concerns, call/drop a message to 9999919685 </p>
         <h1>Thanks for shopping with us! Have a great day :) </h1>
       </div>
     </div>
